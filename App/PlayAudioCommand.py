c9a32d9955ed8f25e9a9c1bf94028617275de28f
Anas-Mhameed <mhameedanaskhader@gmail.com>
2025-05-10 12:31:04 +0300

    removed priority attribute

--- App/PlayAudioCommand.py
+++ App/PlayAudioCommand.py
@@ -1,8 +1,8 @@
 from AudioPriority import AudioPriority
 
 class PlayAudioCommand:
-    def __init__(self, requester: str, file_path: str, priority: AudioPriority):
+    def __init__(self, requester: str, file_path: str):
         self.requester = requester
         self.file_path = file_path
-        self.priority = priority
+        # self.priority = priority
 
