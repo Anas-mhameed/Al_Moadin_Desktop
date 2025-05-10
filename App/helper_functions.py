8e85335d783333823b5eb81ac05798de11c79949
Anas-Mhameed <mhameedanaskhader@gmail.com>
2025-05-10 12:25:32 +0300

    creating a file for shared functions and moving select file function to it

---
+++ App/helper_functions.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QFileDialog 
+
+def select_sound_file(widget):
+        file_dialog = QFileDialog(widget)
+        file_dialog.setNameFilter("Sound Files (*.wav *.mp3 *.ogg)")
+        if file_dialog.exec_():
+            file_paths = file_dialog.selectedFiles()
+            if file_paths:
+                return file_paths[0]
